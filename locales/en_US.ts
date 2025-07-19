c6821953f4e1d282486f8a1b91e81555c88e5718
Dawson <bendawson0924@gmail.com>
2025-07-20 01:24:59 +0800

    v0.1.2

--- locales/en_US.ts
+++ locales/en_US.ts
@@ -3,47 +3,47 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="main.py" line="319"/>
+        <location filename="main.py" line="341"/>
         <source>Language</source>
         <translation>Locale</translation>
     </message>
     <message>
-        <location filename="main.py" line="318"/>
+        <location filename="main.py" line="340"/>
         <source>MouseFlightControl</source>
         <translation>Mouse Flight Control</translation>
     </message>
     <message>
-        <location filename="main.py" line="381"/>
+        <location filename="main.py" line="403"/>
         <source>Start</source>
         <translation>Start</translation>
     </message>
     <message>
-        <location filename="main.py" line="325"/>
+        <location filename="main.py" line="347"/>
         <source>Title</source>
         <translation>Config</translation>
     </message>
     <message>
-        <location filename="main.py" line="382"/>
+        <location filename="main.py" line="404"/>
         <source>StatusStopped</source>
         <translation>Status:stopped</translation>
     </message>
     <message>
-        <location filename="main.py" line="365"/>
+        <location filename="main.py" line="387"/>
         <source>StatusWorking</source>
         <translation>Status:working</translation>
     </message>
     <message>
-        <location filename="main.py" line="328"/>
+        <location filename="main.py" line="350"/>
         <source>CurrentValue</source>
         <translation>Current value</translation>
     </message>
     <message>
-        <location filename="main.py" line="364"/>
+        <location filename="main.py" line="386"/>
         <source>Stop</source>
         <translation>Stop</translation>
     </message>
     <message>
-        <location filename="main.py" line="327"/>
+        <location filename="main.py" line="349"/>
         <source>Sensitive</source>
         <translation>Sensitive</translation>
     </message>
--- locales/zh_CN.ts
+++ locales/zh_CN.ts
@@ -3,47 +3,47 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="main.py" line="319"/>
+        <location filename="main.py" line="341"/>
         <source>Language</source>
         <translation>语言</translation>
     </message>
     <message>
-        <location filename="main.py" line="318"/>
+        <location filename="main.py" line="340"/>
         <source>MouseFlightControl</source>
         <translation>鼠标飞行控制软件</translation>
     </message>
     <message>
-        <location filename="main.py" line="381"/>
+        <location filename="main.py" line="403"/>
         <source>Start</source>
         <translation>开始</translation>
     </message>
     <message>
-        <location filename="main.py" line="325"/>
+        <location filename="main.py" line="347"/>
         <source>Title</source>
         <translation>配置</translation>
     </message>
     <message>
-        <location filename="main.py" line="382"/>
+        <location filename="main.py" line="404"/>
         <source>StatusStopped</source>
         <translation>状态：停止</translation>
     </message>
     <message>
-        <location filename="main.py" line="365"/>
+        <location filename="main.py" line="387"/>
         <source>StatusWorking</source>
         <translation>状态：工作中</translation>
     </message>
     <message>
-        <location filename="main.py" line="328"/>
+        <location filename="main.py" line="350"/>
         <source>CurrentValue</source>
         <translation>当前值</translation>
     </message>
     <message>
-        <location filename="main.py" line="364"/>
+        <location filename="main.py" line="386"/>
         <source>Stop</source>
         <translation>停止</translation>
     </message>
     <message>
-        <location filename="main.py" line="327"/>
+        <location filename="main.py" line="349"/>
         <source>Sensitive</source>
         <translation>鼠标灵敏度</translation>
     </message>
